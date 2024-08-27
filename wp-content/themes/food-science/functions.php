cf905378d479bca4e753d6b943a8ff955803ed80
mokaruku <web.it.reico29@gmail.com>
2024-08-27 12:46:53 +0900

    フード一覧ページを追加、フード詳細ページとACFでカスタムフィールドを追加

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -1,14 +1,10 @@
 <?php
-
 // タイトルタグを有効化する
 add_theme_support('title-tag');
-
 // アイキャッチ画像を有効化する
 add_theme_support('post-thumbnails');
-
-//menuを有効化するカスタムメニュー機能を使用可能にする
+// メニューを有効化する
 add_theme_support('menus');
-
 // titleタグの区切り文字を変更する
 add_filter('document_title_separator', 'my_document_title_separator');
 function my_document_title_separator($separator)
@@ -16,22 +12,18 @@ function my_document_title_separator($separator)
   $separator = '|';
   return $separator;
 }
-
-// ContactForm7の出力の際にｐタグが勝手に入らないようにする
+// Contact Form 7の出力の際にPタグが勝手に入らないようにする
 add_filter('wpcf7_autop_or_not', 'my_wpcf7_autop');
 function my_wpcf7_autop()
 {
   return false;
 }
-
-
-//ショートコードテスト
-//ショートコード用関数
-
+// ショートコードテスト
+// ショートコード用関数
 // $test = [
 //   'text' => 'あいうえお',
+//   'link' => 'https://google.co.jp',
 // ];
-
 function shortcode_test($test, $content = null)
 {
   $default = [
@@ -39,31 +31,56 @@ function shortcode_test($test, $content = null)
     'link' => '',
   ];
   $test = shortcode_atts($default, $test);
-
-
   $html = $content . 'ショートコードテスト' . $test['text'] . "({$test['link']})";
-
   return $html;
 }
-
-//ショートコードの登録
-//add_shortcode('ショートコード名', '呼び出す関数名');
+// ショートコードの登録
+// add_shortcode('ショートコード名', '呼び出す関数名');
 add_shortcode('sc-test', 'shortcode_test');
-
-
 /**
  * メインクエリを変更する
  */
 add_action('pre_get_posts', 'my_pre_get_posts');
 function my_pre_get_posts($query)
 {
-  //管理画面、メインクエリ以外には設定しない
+  // 管理画面、メインクエリ以外は実行しない
   if (is_admin() || !$query->is_main_query()) {
     return;
   }
-  //トップページの場合
+  // トップページ
   if ($query->is_home()) {
     $query->set('posts_per_page', 3);
     return;
   }
 }
+
+
+/**
+ * パスワード保護中のページのタイトルから「保護中」の文字を削除する
+ * 
+ */
+add_filter('protected_title_format', 'my_protected_title');
+function my_protected_title()
+{
+  return '%s';
+}
+
+/**
+ * パスワード保護フォームをカスタマイズする
+ */
+function my_password_form()
+{
+  remove_filter('the_content', 'wpautop');
+
+  $wp_login_url = wp_login_url();
+  $html = <<<HTML
+  <p>パスワードを入力してください。</p>
+  <form action="{$wp_login_url}" class="post-password-form" method="post">
+    <input name="post_password" type="password">
+    <input type="submit" name="Submit" value="送信">
+</form>
+HTML;
+
+  return $html;
+}
+add_filter('the_password_form', 'my_password_form');
